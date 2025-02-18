c19ba51ca83cd598e50704d0ddaa3843bf8e9e98
Ritesh_Dwivedi__57 <dritesh656@gmail.com>
2025-02-18 16:35:23 +0530

    Update 01.js

--- Week-2/Task-1/01.js
+++ Week-2/Task-1/01.js
@@ -1,1 +1,13 @@
+Write a function that takes an input of item prices and returns the total bill.
 
+function calculateTotal(prices) 
+{
+    
+    return prices.reduce((total, price) => total + price, 0);
+}
+let itemPrices = [10.50, 20.99, 5.75, 12.30]; 
+let totalBill = calculateTotal(itemPrices);
+
+console.log("Total Bill: $" + totalBill.toFixed(2));
+
+  
