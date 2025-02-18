9b08ac8cddd82d00c6ac0f16f20ec11a0e552dc9
Ritesh_Dwivedi__57 <dritesh656@gmail.com>
2025-02-18 16:37:11 +0530

    Update 03.js

--- Week-2/Task-1/03.js
+++ Week-2/Task-1/03.js
@@ -1,1 +1,12 @@
+Find the Largest Number in an Array.
 
+function findLargestNumber(arr) {
+    if (arr.length === 0) {
+        return "Array is empty"; 
+    }
+    return Math.max(...arr); 
+}
+let numbers = [12, 45, 67, 89, 23, 56];
+let largestNumber = findLargestNumber(numbers);
+
+console.log("The largest number is:", largestNumber);
