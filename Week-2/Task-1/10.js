367b0f619635626356b89ebfd0e7a55bd03e4b79
Ritesh_Dwivedi__57 <dritesh656@gmail.com>
2025-02-18 16:44:14 +0530

    Update 10.js

--- Week-2/Task-1/10.js
+++ Week-2/Task-1/10.js
@@ -1,1 +1,19 @@
+            Wite a program to print pattern.
+1  
+1 2  
+1 2 3  
+1 2 3 4  
+1 2 3 4 5  
 
+Code. 
+  
+function printPattern(rows) {
+    for (let i = 1; i <= rows; i++) {
+        let pattern = "";
+        for (let j = 1; j <= i; j++) {
+            pattern += j + " ";
+        }
+        console.log(pattern.trim());
+    }
+}
+printPattern(5);
