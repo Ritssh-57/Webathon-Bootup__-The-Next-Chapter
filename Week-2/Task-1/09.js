0969119798f0037df2fd15450ec9d06e005537c5
Ritesh_Dwivedi__57 <dritesh656@gmail.com>
2025-02-18 16:43:30 +0530

    Update 09.js

--- Week-2/Task-1/09.js
+++ Week-2/Task-1/09.js
@@ -1,1 +1,16 @@
+Write a JavaScript function that checks whether a number is prime.
 
+  function isPrime(num)
+  {
+    if (num < 2) return false; 
+
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+let number = 29;
+console.log(number + " is prime? " + isPrime(number));
