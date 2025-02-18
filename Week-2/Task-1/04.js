77697e8dfa8dbb20eb31801776a1c3e20991659d
Ritesh_Dwivedi__57 <dritesh656@gmail.com>
2025-02-18 16:38:01 +0530

    Update 04.js

--- Week-2/Task-1/04.js
+++ Week-2/Task-1/04.js
@@ -1,1 +1,15 @@
+Write a javascript code that calculates the squares and cubes from 0 to 10 .
 
+  function calculateSquaresAndCubes()
+  {
+    console.log("Number | Square | Cube");
+    console.log("----------------------");
+    
+    for (let i = 0; i <= 10; i++) 
+    {
+        let square = i ** 2; 
+        let cube = i ** 3;    
+        console.log(`${i}      | ${square}      | ${cube}`);
+    }
+}
+calculateSquaresAndCubes();
